2c9705861b5e9b8141064c38c6b29e2c239a0e7a
Satish Babariya <satish.babariya@gmail.com>
2019-04-28 13:41:00 +0530

    monaco editor added

---
+++ client/src/index.ts
@@ -0,0 +1,26 @@
+import { Widget } from '@phosphor/widgets';
+import { SplitPanel } from '@phosphor/widgets';
+import { Editor } from "./widgets/editor";
+import './styles/index.css';
+
+function main(): void {
+
+    let main = new SplitPanel({
+        orientation : 'vertical',
+        spacing: 4
+      })
+      main.id = 'main';
+
+      let editor : Editor = new Editor();
+      let editor2 : Editor = new Editor();
+
+      main.addWidget(editor);
+      main.addWidget(editor2);
+
+      window.onresize = () => { main.update(); };
+
+      Widget.attach(main, document.body);
+
+}
+
+window.onload = main;
---
+++ client/src/widgets/editor.ts
@@ -0,0 +1,46 @@
+import { Widget } from '@phosphor/widgets';
+import * as monaco from 'monaco-editor';
+import '../styles/editor.css';
+
+export class Editor extends Widget {
+
+    editor: monaco.editor.IStandaloneCodeEditor;
+
+    constructor() {
+        super();
+        this.addClass('editor');
+        this.title.label = 'main.swift';
+        this.title.closable = true;
+        this.editor = monaco.editor.create(this.node, {
+          language: 'swift'
+        });
+      }
+
+      addWidget(widget: Widget) {
+        this.node.appendChild(widget.node);
+      }
+    
+      onResize() {
+        this.editor.layout();
+      }
+    
+      onAfterShow() {    
+        this.editor.layout();
+      }
+    
+      onCloseRequest() {
+        this.dispose();
+      }
+    
+      public onToggleTheme(theme : string) {
+        monaco.editor.setTheme(theme);
+      }
+    
+      public dispose() {
+        if (this.isDisposed) {
+            return;
+        }
+        this.editor.dispose();
+        super.dispose();
+    }
+}
