685627963c61900db873a27cdfd06b048824ff95
Satish Babariya <satish.babariya@gmail.com>
2019-04-28 12:12:22 +0530

    server code added

---
+++ client/webpack.config.ts
@@ -0,0 +1,45 @@
+import * as webpack from "webpack";
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+
+module.exports = {
+    entry: {
+      index: './src/index.ts'
+    },
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist/')
+    },
+    resolve: {
+      modules: [path.resolve(__dirname), 'node_modules'],
+      extensions: ['.ts', '.js']
+    },
+    module: {
+      rules: [
+        { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+        { test: /\.ts$/, use: ['ts-loader', 'tslint-loader']},
+      ]
+    },
+    plugins: [
+      
+      new HtmlWebpackPlugin({
+        title: "Swift Playground"
+      }),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        minChunks: function (module) {
+          return module.context && module.context.indexOf('node_modules') !== -1;
+        }
+      }),
+      new MonacoWebpackPlugin({
+        languages: ['swift']
+      })
+    ],
+    devServer: {
+      contentBase: path.join(__dirname, "dist"),
+      port: 8080,
+      open: true
+    }
+  };
---
+++ server/src/swift-server-launcher.ts
@@ -0,0 +1,27 @@
+import * as rpc from "vscode-ws-jsonrpc";
+import * as server from "vscode-ws-jsonrpc/lib/server";
+import * as lsp from "vscode-languageserver";
+
+export function launch(socket: rpc.IWebSocket) {
+  const reader = new rpc.WebSocketMessageReader(socket);
+  const writer = new rpc.WebSocketMessageWriter(socket);
+
+  const socketConnection = server.createConnection(reader, writer, () =>
+    socket.dispose()
+  );
+  const serverConnection = server.createServerProcess(
+    "Sourcekit-LSP",
+    "sourcekit-lsp",
+    []
+  );
+
+  server.forward(socketConnection, serverConnection, message => {
+    if (rpc.isRequestMessage(message)) {
+      if (message.method === lsp.InitializeRequest.type.method) {
+        const initializeParams = message.params as lsp.InitializeParams;
+        initializeParams.processId = process.pid;
+      }
+    }
+    return message;
+  });
+}
